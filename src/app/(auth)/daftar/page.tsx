2dfec5caf3ce2fc4720f69be6071a822acd6a387
Rahmat <rahmatsuhadi32@gmail.com>
2025-08-24 07:38:51 +0700

    feat: add integration api ini masuk and register page

--- src/app/(auth)/daftar/page.tsx
+++ src/app/(auth)/daftar/page.tsx
@@ -2,11 +2,9 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
-import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -19,61 +17,64 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import {  Eye, EyeOff } from 'lucide-react';
+import { Eye, EyeOff, Mail, User2 } from 'lucide-react';
 import { FaPhoneAlt } from 'react-icons/fa';
 import { IoLockClosed } from "react-icons/io5";
+import { useRegister } from '@/features/auth/hooks';
+import { Illustration1, Illustration2 } from '@/components/auth/IllustrasiImages';
+import { withMask } from 'use-mask-input';
 const formSchema = z.object({
-  phone: z.string().min(1, {
-    message: "Nomor HP tidak boleh kosong.",
+  phone_number: z.string().min(1, { message: "Nomor HP tidak boleh kosong." }).transform((val) => val.replace(/\s+/g, '')) // hapus semua spasi dulu
+    .refine((val) => /^08[0-9]{8,12}$/.test(val), {
+      message: 'Nomor harus diawali 08 dan panjangnya 10-14 digit',
+    }),
+  // .min(1, { message: "Nomor HP tidak boleh kosong." })
+  // .regex(/^\+62[0-9]{9,13}$/, {
+  //   message: "Format nomor HP tidak valid (gunakan format +62...).",
+  // }),
+  name: z.string().min(1, {
+    message: "Nama tidak boleh kosong.",
   }),
   password: z.string().min(1, {
     message: "Password tidak boleh kosong.",
   }),
-  passwordAgain: z.string().min(1, {
+  password_confirmation: z.string().min(1, {
     message: "Konfirmasi Password tidak boleh kosong.",
   }),
+  email: z.string()
+    .min(1, { message: "Email tidak boleh kosong." })
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, {
+      message: "Format email tidak valid.",
+    }),
 });
 
 export default function DaftarPage() {
-  const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
   const [showPasswordAgain, setShowPasswordAgain] = useState(false);
+
+  const { mutate: handleRegister, isPending } = useRegister();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      phone: "",
+      phone_number: "",
+      email: "",
       password: "",
-      passwordAgain: ""
+      password_confirmation: "",
+      name: ""
     },
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values); // Data yang akan dikirim ke API
-
-    try {
-      // Di sini Anda akan memanggil API login Anda
-      // const response = await apiClient.post('/auth/login', values);
-
-      // Simulasi panggilan API berhasil
-      await new Promise(resolve => setTimeout(resolve, 1000));
-
-      toast.success("Login Berhasil!", {
-        description: "Selamat datang kembali.",
-      });
-
-      // Arahkan pengguna ke halaman dashboard setelah berhasil login
-      router.push('/dashboard');
-
-    } catch (error) {
-      toast.error("Login Gagal", {
-        description: "Nomor HP atau password salah. Silakan coba lagi.",
-      });
-    }
+    handleRegister(values)
   }
 
   return (
-    <div className="flex items-center justify-center min-h-[80vh] px-4   font-jakarta">
+    <div className="flex items-center relative justify-center min-h-[80vh] px-4   font-jakarta">
+
+      <div className="hidden lg:block absolute left-0 max-w-xs xl:max-w-sm 2xl:max-w-md">
+        <Illustration1 className="w-full h-auto" />
+      </div>
       <Card className="w-full max-w-md">
         <CardHeader className="text-center">
           <CardTitle className="text-3xl font-bold text-[#36454F]">Daftar</CardTitle>
@@ -84,14 +85,54 @@ export default function DaftarPage() {
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
               <FormField
                 control={form.control}
-                name="phone"
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nama</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <User2 className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                        <Input placeholder="Amanda Sekar" className="pl-10 py-5 rounded-xl" {...field} />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Mail className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                        <Input type='email' placeholder="amanda@gmail.com" className="pl-10 py-5 rounded-xl" {...field} />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone_number"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Nomor HP</FormLabel>
                     <FormControl>
                       <div className="relative">
-                        <FaPhoneAlt  className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
-                        <Input placeholder="0812 xxxxxx" className="pl-10 py-5 rounded-xl" {...field} />
+                        <FaPhoneAlt className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                        <Input
+                          {...field}
+                          placeholder="08xx xxxx xxxx"
+                          className="pl-10 py-5 rounded-xl pr-10"
+                          ref={withMask('999 9999 9999 999999', {
+                            placeholder: '',
+                            showMaskOnHover: false
+                          })}
+                        />
                       </div>
                     </FormControl>
                     <FormMessage />
@@ -106,7 +147,7 @@ export default function DaftarPage() {
                     <FormLabel>Password</FormLabel>
                     <FormControl>
                       <div className="relative">
-                        <IoLockClosed  className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                        <IoLockClosed className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
                         <Input
                           type={showPassword ? 'text' : 'password'}
                           placeholder="********"
@@ -126,15 +167,15 @@ export default function DaftarPage() {
                   </FormItem>
                 )}
               />
-               <FormField
+              <FormField
                 control={form.control}
-                name="passwordAgain"
+                name="password_confirmation"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Konfirmasi Password</FormLabel>
                     <FormControl>
                       <div className="relative">
-                        <IoLockClosed  className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+                        <IoLockClosed className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
                         <Input
                           type={showPasswordAgain ? 'text' : 'password'}
                           placeholder="********"
@@ -154,8 +195,8 @@ export default function DaftarPage() {
                   </FormItem>
                 )}
               />
-              <Button type="submit" className="w-full py-6" disabled={form.formState.isSubmitting}>
-                {form.formState.isSubmitting ? 'Memproses...' : 'Daftar'}
+              <Button type="submit" className="w-full py-6" disabled={isPending}>
+                {isPending ? 'Memproses...' : 'Daftar'}
               </Button>
             </form>
           </Form>
@@ -167,6 +208,10 @@ export default function DaftarPage() {
           <Link href={"/masuk"} className='text-primary underline'>Masuk</Link>
         </CardFooter>
       </Card>
+
+      <div className="hidden lg:block absolute right-0 max-w-xs xl:max-w-sm 2xl:max-w-md">
+        <Illustration2 className="w-full h-auto" />
+      </div>
     </div>
   );
 }
