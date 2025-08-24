5d3c363960eba2fe715ea44ffa8687ef31d3bbf9
Rahmat <rahmatsuhadi32@gmail.com>
2025-08-24 07:14:45 +0700

    feat: add api and hook categories

---
+++ src/features/categories/api.ts
@@ -0,0 +1,10 @@
+
+import { apiClient } from "@/lib/api-client";
+import type { Category } from "@/types";
+
+/**
+ * Mengambil semua kategori dari server.
+ */
+export const getCategories = (): Promise<{data:Category[]}> => {
+  return apiClient<{data:Category[]}>('/categories');
+};
