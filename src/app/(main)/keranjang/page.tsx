b998f21a2d8ab556181c05237ddefcaeb8accd10
Rahmat <rahmatsuhadi32@gmail.com>
2025-08-24 07:48:08 +0700

    feat: add integration data in  cart page

--- src/app/(main)/keranjang/page.tsx
+++ src/app/(main)/keranjang/page.tsx
@@ -1,34 +1,38 @@
 "use client";
 import { StoreCartItem } from "@/components/cart/card";
-import Footer from "@/components/layout/Footer";
+import ContactSection from "@/components/landing/Contact";
+import { AnimatedWrapper } from "@/components/shared/AnimateWrapper";
+import { useCartItems, } from "@/features/cart/hooks";
+import { CartItem } from "@/types";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
 
 export default function CartPage() {
 
-    const [cartItems, setCartItems] = React.useState([
-        { title: 'Keripik Tempe Original', variant: 'Original', price: 15000, quantity: 2 },
-        { title: 'Kerupuk Tahu Krispy', variant: 'Original', price: 12000, quantity: 1 }
-    ]);
 
-    const handleItemIncrement = (index: number) => {
-        const updatedItems = [...cartItems];
-        updatedItems[index].quantity += 1;
-        setCartItems(updatedItems);
-    };
+    const { data } = useCartItems()
 
-    const handleItemDecrement = (index: number) => {
-        const updatedItems = [...cartItems];
-        if (updatedItems[index].quantity > 1) {
-            updatedItems[index].quantity -= 1;
-            setCartItems(updatedItems);
-        }
-    };
 
-    const handleItemRemove = (index: number) => {
-        const updatedItems = cartItems.filter((_, i) => i !== index);
-        setCartItems(updatedItems);
-    };
+    const cartItems = useMemo(() => data?.data || [], [data?.data]);
+
+
+    const groupedItems = useMemo(() => {
+        if (!cartItems) return {};
+
+        return cartItems.reduce((acc, item) => {
+            const storeId = item.store_id; // Asumsi store_id ada di dalam data produk
+            // const storeName = item.product.store.name;
+
+            const key = `${storeId}`; // Buat key unik dengan nama toko
+
+            if (!acc[key]) {
+                acc[key] = [];
+            }
+            acc[key].push(item);
+            return acc;
+        }, {} as Record<string, CartItem[]>);
+    }, [cartItems]);
+
     return (
         <div className="bg-gray-50">
             {/* header */}
@@ -42,10 +46,6 @@ export default function CartPage() {
                                 <span className="hidden sm:inline">Kembali Belanja</span>
                                 <span className="sm:hidden">Belanja</span>
                             </Link>
-                            {/* <Link href="/" className="text-primary hover:text-primary-dark text-sm sm:text-base">
-                                <i className="fas fa-home mr-1"></i>
-                                <span className="hidden sm:inline">Beranda</span>
-                            </Link> */}
                         </div>
                     </div>
                 </div>
@@ -53,25 +53,18 @@ export default function CartPage() {
 
             <section className="py-8">
                 <div className="container mx-auto px-4">
-                    <StoreCartItem
-                        storeName="Toko Cemilan Sleman"
-                        storeLocation="Jl. Magelang KM 5, Sleman"
-                        shipping={10000}
-                        items={cartItems}
-                        onItemIncrement={handleItemIncrement}
-                        onItemDecrement={handleItemDecrement}
-                        onItemRemove={handleItemRemove}
-                    />
+                    {Object.entries(groupedItems).map(([storeId, storeItems]) => {
+                        return (
+                            <StoreCartItem
+                                key={storeId}
+                                storeName={storeId}
+                                storeLocation="Jl. Magelang KM 5, Sleman"
+                                shipping={10000}
+                                items={storeItems}
 
-                    <StoreCartItem
-                        storeName="Toko Cemilan Sleman"
-                        storeLocation="Jl. Magelang KM 5, Sleman"
-                        shipping={10000}
-                        items={cartItems}
-                        onItemIncrement={handleItemIncrement}
-                        onItemDecrement={handleItemDecrement}
-                        onItemRemove={handleItemRemove}
-                    />
+                            />
+                        )
+                    })}
 
                     <div className="bg-white rounded-lg shadow-md p-4 sm:p-6 mt-6">
                         <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center space-y-4 sm:space-y-0">
@@ -85,7 +78,7 @@ export default function CartPage() {
                             <div className="text-left sm:text-right">
                                 <p className="text-lg sm:text-2xl font-bold text-gray-800">Total Keranjang</p>
                                 <p className="text-2xl sm:text-3xl font-bold text-primary">
-                                    Rp {cartItems.reduce((acc: number, item) => acc + item.price * item.quantity, 0) + 10000}
+                                    {/* Rp {cartItems.reduce((acc: number, item) => acc + item.price * item.quantity, 0) + 10000} */}
                                 </p>
                                 <p className="text-xs sm:text-sm text-gray-500">*Belum termasuk ongkir</p>
                             </div>
@@ -94,7 +87,9 @@ export default function CartPage() {
                 </div>
             </section>
 
-            <Footer />
+            <AnimatedWrapper>
+                <ContactSection />
+            </AnimatedWrapper>
         </div>
     );
 }
