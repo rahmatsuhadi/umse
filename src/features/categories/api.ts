122defbb63b1fe1f4acd2453ae407ef544d0f93d
Rahmat <rahmatsuhadi32@gmail.com>
2025-08-25 08:17:31 +0700

    feat: add filter categories isfetured

--- src/features/categories/api.ts
+++ src/features/categories/api.ts
@@ -6,5 +6,5 @@ import type { Category } from "@/types";
  * Mengambil semua kategori dari server.
  */
 export const getCategories = (): Promise<{data:Category[]}> => {
-  return apiClient<{data:Category[]}>('/categories');
+  return apiClient<{data:Category[]}>('/categories?filter[is_featured]=true');
 };
