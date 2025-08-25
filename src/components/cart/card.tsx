a0706e1e7d9831d3253a8674f3ef95319edfd6b9
Rahmat <rahmatsuhadi32@gmail.com>
2025-08-25 08:22:23 +0700

    chore: change prop in CartItemCard

--- src/components/cart/card.tsx
+++ src/components/cart/card.tsx
@@ -2,26 +2,30 @@
 import { Checkbox } from "../ui/checkbox";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { CartItem } from "@/types";
+import { CartItem, Price } from "@/types";
 import { useRemoveCartItem, useUpdateCartItem } from "@/features/cart/hooks";
+import Image from "next/image";
+import { formatRupiah } from "@/lib/curency-format";
 
 
 
 interface CartItemProps {
     title: string;
     variant: string;
-    price: number;
+    price: Price;
     quantity: number;
     onIncrement: () => void;
     onDecrement: () => void;
     onRemove: () => void;
     isChecked: boolean;  // Tambahkan ini untuk mengontrol status checkbox
     onCheck: () => void; // Fungsi untuk mengubah status checkbox item
+    media: string
 }
 
 export const CartItemCard = ({
     title,
     variant,
+    media,
     price,
     quantity,
     onIncrement,
@@ -38,12 +42,13 @@ export const CartItemCard = ({
                 className="mr-3 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary"
             />
             <div className="bg-gray-200 rounded w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center mr-3 sm:mr-4 flex-shrink-0">
-                <i className="fas fa-image text-gray-400"></i>
+                {/* <i className="fas fa-image text-gray-400"></i> */}
+                <Image src={media} width={100} height={100} alt="thumb-product" />
             </div>
             <div className="flex-1 min-w-0">
                 <h4 className="font-bold text-gray-800 text-sm sm:text-base">{title}</h4>
                 <p className="text-xs sm:text-sm text-gray-600">Varian: {variant}</p>
-                <p className="text-primary font-bold text-sm sm:text-base">Rp {price}</p>
+                <p className="text-primary font-bold text-sm sm:text-base">{price.formatted}</p>
             </div>
         </div>
         <div className="flex items-center justify-between sm:justify-end space-x-3 sm:space-x-4">
@@ -57,7 +62,7 @@ export const CartItemCard = ({
                 </button>
             </div>
             <div className="text-right">
-                <p className="font-bold text-gray-800 text-sm sm:text-base">Rp {price * quantity}</p>
+                <p className="font-bold text-gray-800 text-sm sm:text-base">{formatRupiah(price.value * quantity)}</p>
             </div>
             <button className="text-red-500 hover:text-red-700" onClick={onRemove}>
                 <i className="fas fa-trash text-sm"></i>
@@ -69,24 +74,28 @@ export const CartItemCard = ({
 interface StoreCartItemProps {
     storeName: string;
     storeLocation: string;
-    shipping: number;
+    // shipping: number;
+    img:string;
+    storeId: string;
     items: CartItem[];
 }
 
 
 export const StoreCartItem = ({
     storeName,
+    storeId,
     storeLocation,
-    shipping,
+    img,
+    // shipping,
     items,
 }: StoreCartItemProps) => {
     const [isStoreChecked, setIsStoreChecked] = useState(false); // State untuk checkbox store
     const [itemChecked, setItemChecked] = useState<boolean[]>(new Array(items.length).fill(false)); // State untuk checkbox item
 
+    const selectedItems = items.filter((_, idx) => itemChecked[idx]);
 
     const { mutate: updateItem, } = useUpdateCartItem();
-    const { mutate: removeItem,  } = useRemoveCartItem();
-
+    const { mutate: removeItem, } = useRemoveCartItem();
 
     // Mengubah status checkbox store dan checkbox item
     const handleStoreCheckboxChange = (checked: boolean) => {
@@ -110,7 +119,7 @@ export const StoreCartItem = ({
         if (newQuantity >= 1) {
             updateItem({
                 item_id: item.id,
-                quantity: newQuantity,
+                quantity: 1,
                 variant_id: item.variant_id
             });
         } else {
@@ -122,7 +131,13 @@ export const StoreCartItem = ({
 
     const onCheckout = () => {
         // Logika checkout untuk toko ini
-        router.push("/pembayaran");
+
+
+        if (selectedItems.length === 0) return;
+
+        localStorage.setItem("checkout_items", JSON.stringify(selectedItems));
+
+        router.push("/pembayaran?store=" + storeId);
     }
 
     return (
@@ -135,7 +150,8 @@ export const StoreCartItem = ({
                         className="mr-3 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary"
                     />
                     <div className="bg-primary rounded-full w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center mr-3">
-                        <i className="fas fa-store text-white text-sm sm:text-base"></i>
+                        {/* <i className="fas fa-store text-white text-sm sm:text-base"></i> */}
+                        <Image src={img} className='rounded-full' alt='store-img' width={200} height={200}/>
                     </div>
                     <div>
                         <h3 className="font-bold text-gray-800 text-sm sm:text-base">{storeName}</h3>
@@ -152,10 +168,11 @@ export const StoreCartItem = ({
                 {items.map((item, index) => (
                     <CartItemCard
                         key={index}
-                        title={item.id}
-                        variant={item.variant_id || 'Tanpa Variant'}
-                        price={1000011}
+                        title={item.product.name}
+                        variant={item.variant?.name || 'Tanpa Variant'}
+                        price={item.variant?.price || item.product.price}
                         quantity={item.quantity}
+                        media={item.product.thumbnail.media_url}
                         onIncrement={() => handleQuantityChange(item, +1)}
                         onDecrement={() => handleQuantityChange(item, -1)}
                         onRemove={() => removeItem(item.id)}
@@ -166,8 +183,8 @@ export const StoreCartItem = ({
 
                 <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center pt-4 space-y-3 sm:space-y-0">
                     <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-4 space-y-1 sm:space-y-0">
-                        {/* <span className="text-xs sm:text-sm text-gray-600">Subtotal Produk: Rp {items.reduce((acc, item) => acc + item.price * item.quantity, 0)}</span> */}
-                        <span className="text-xs sm:text-sm text-gray-600">Ongkir: Rp {shipping}</span>
+                        <span className="text-xs sm:text-sm text-gray-600">Subtotal Produk: {formatRupiah(selectedItems.reduce((acc, item) => acc + (item.variant?.price.value || item.product.price.value) * item.quantity, 0))}</span>
+                        {/* <span className="text-xs sm:text-sm text-gray-600">Ongkir: Rp {shipping}</span> */}
                     </div>
                     <div className="text-left sm:text-right">
                         {/* <p className="text-base sm:text-lg font-bold text-gray-800">Total: Rp {items.reduce((acc, item) => acc + item.price * item.quantity, 0) + shipping}</p> */}
