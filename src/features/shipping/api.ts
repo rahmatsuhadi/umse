7c1f18ec97dbd837209e72603cc3d23076335948
Rahmat <rahmatsuhadi32@gmail.com>
2025-08-28 15:59:39 +0700

    feat: add api hooks and types shippings

---
+++ src/features/shipping/api.ts
@@ -0,0 +1,14 @@
+import { apiClient } from "@/lib/api-client";
+import { ResponseShippingRates, ShippingRates } from "@/types";
+
+
+
+/**
+ * Mengambil satu produk berdasarkan id-nya.
+ */
+export const getShippingRates = (data: ShippingRates): Promise<{ data: ResponseShippingRates, message: string }> => {
+    return apiClient<{ data: ResponseShippingRates, message: string }>(`/shipping/rates`, {
+        method: "POST",
+        body: JSON.stringify(data),
+    });
+};
---
+++ src/features/shipping/hooks.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getShippingRates } from "./api";
+import { ItemShipping, ResponseShippingRates } from "@/types";
+
+
+export const useShippingRates = ({ 
+    destination_village_id, 
+    items, 
+    origin_village_id 
+}: { destination_village_id?: number, items: ItemShipping[], origin_village_id?: number }) => {
+    return useQuery<{ data: ResponseShippingRates }, Error>({
+        queryKey: ["shippingRates", destination_village_id,origin_village_id ], // Kunci query unik untuk produk ini
+        refetchOnReconnect:false,
+        refetchOnWindowFocus: false,
+        queryFn: () => getShippingRates({
+            destination_village_id, items, origin_village_id
+        }),
+        enabled: !!destination_village_id && !!origin_village_id, // Hanya jalankan query jika slug ada
+    });
+};
