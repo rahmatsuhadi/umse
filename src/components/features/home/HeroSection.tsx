a7f22dfc5fad8b79be3262535d07665a7b079451
Rahmat <rahmatsuhadi32@gmail.com>
2025-08-29 08:40:05 +0700

    feat: add slide auto in hero section

--- src/components/features/home/HeroSection.tsx
+++ src/components/features/home/HeroSection.tsx
@@ -1,8 +1,23 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import EmblaCarouselFade from "embla-carousel-fade"; // <-- Impor plugin fade
 import Image from "next/image";
 import Link from "next/link";
 
 export default function HeroSection() {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({delay: 3000}), EmblaCarouselFade({active: true})])
+
+
+  // Array of images to display in the slider
+  const sliderImages = [
+    { src: "/hero/images1.jpg", alt: "Workspace" },
+    { src: "/hero/images2.jpg", alt: "Produk UMKM" }, // Ganti dengan gambar Anda
+    { src: "/hero/images3.jpg", alt: "Layanan Digital" }, // Ganti dengan gambar Anda
+  ];
+
+
   return (
     <section className="bg-[#FFE6C8] ">
       <div className="grid grid-cols-1 md:grid-cols-2 md:min-h-[87vh] min-h-[50vh]">
@@ -16,22 +31,41 @@ export default function HeroSection() {
           </p>
           <div className="mt-8">
             <Link href={"/"}>
-            <Button type="button" className="bg-primary rounded-sm px-9 py-7 hover:cursor-pointer hover:bg-primary/50">
-              Mulai Belanja
-            </Button>
+              <Button type="button" className="bg-primary rounded-sm px-9 py-7 hover:cursor-pointer hover:bg-primary/50">
+                Mulai Belanja
+              </Button>
             </Link>
           </div>
         </div>
 
         {/* Kolom Kanan: Gambar */}
-        <div className="relative h-64 md:h-auto">
+        {/* <div className="relative h-64 md:h-auto  md:rounded-tl-[200px] overflow-hidden">
           <Image
             src="/hero.png"
             alt="Workspace"
             layout="fill"
             objectFit="cover"
-            className="w-full h-full md:rounded-tl-[200px]"
+            className="w-full h-full"
           />
+        </div> */}
+
+        <div className="embla relative h-64 md:h-auto  md:rounded-tl-[200px] overflow-hidden" ref={emblaRef}>
+          <div className="embla__container">
+            {sliderImages.map((item, i) => (
+              <div className="embla__slide relative w-full h-64 md:h-full md:min-h-[87vh]" key={i}>
+
+                <Image
+
+                  src={item.src}
+                  alt={item.alt}
+                  layout="fill"
+                  objectFit="cover"
+                  className="w-full h-full"
+                />
+              </div>
+            ))}
+
+          </div>
         </div>
       </div>
     </section>
