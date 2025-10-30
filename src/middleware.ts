440d0293cbbf6e50acf9b71a8cc49b03311238ba
Rahmat <rahmatsuhadi32@gmail.com>
2025-10-31 06:53:27 +0700

    fix: add production conditioal middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,49 +1,52 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 
 export function middleware(req: Request) {
-  // Generate nonce (Edge runtime-safe)
+  // Generate nonce
   const array = new Uint8Array(16);
   crypto.getRandomValues(array);
   const nonce = Array.from(array)
     .map((b) => b.toString(16).padStart(2, "0"))
     .join("");
 
-  const origin = new URL(req.url).origin;
-  const reportEndpoint = `${origin}/api/csp-report`;
+  const isDev = process.env.NODE_ENV === "development";
+
+  // const origin = new URL(req.url).origin;
+  // const reportEndpoint = `${origin}/api/csp-report`;
 
   const csp = [
     `default-src 'self'`,
-    `script-src 'self' 'nonce-${nonce}' https://cdn.ckeditor.com https://cdnjs.cloudflare.com https://www.google.com https://www.gstatic.com https://www.recaptcha.net`,
+    `script-src 'self' ${
+      isDev ? "'unsafe-eval'" : ""
+    } 'nonce-${nonce}' https://cdn.ckeditor.com https://cdnjs.cloudflare.com https://www.google.com https://www.gstatic.com https://www.recaptcha.net`,
     `style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.ckeditor.com`,
     `img-src 'self' data: https: https://www.google.com https://www.gstatic.com https://www.recaptcha.net`,
     `font-src 'self' data: https://cdnjs.cloudflare.com`,
     `connect-src 'self' https: https://www.google.com https://www.gstatic.com https://www.recaptcha.net`,
     `frame-src https://www.google.com https://www.gstatic.com https://www.recaptcha.net`,
-    `frame-ancestors 'none'`,
+    `object-src 'none'`,
     `base-uri 'self'`,
     `form-action 'self'`,
-    `report-uri ${reportEndpoint}`,
+    `frame-ancestors 'none'`,
+    `upgrade-insecure-requests`,
   ].join("; ");
 
   const res = NextResponse.next();
 
-  // Sementara pakai Report-Only (tidak memblokir skrip tapi kirim laporan)
+  //  Report-Only
   // res.headers.set("Content-Security-Policy-Report-Only", csp);
   res.headers.set("Content-Security-Policy", csp);
 
-  // Optional: kirim metadata ke browser
-  res.headers.set(
-    "Report-To",
-    JSON.stringify({
-      group: "csp-endpoint",
-      max_age: 10886400,
-      endpoints: [{ url: reportEndpoint }],
-      include_subdomains: true,
-    })
-  );
+  // res.headers.set(
+  //   "Report-To",
+  //   JSON.stringify({
+  //     group: "csp-endpoint",
+  //     max_age: 10886400,
+  //     endpoints: [{ url: reportEndpoint }],
+  //     include_subdomains: true,
+  //   })
+  // );
 
-  // Kirim nonce ke layout agar inline script aman
+  // Kirim nonce ke layout
   res.headers.set("x-nonce", nonce);
 
   return res;
