9ea12cf70b67f6d9bf521c24e1ec94ce8ded74d0
alif <alifdwi136@gmail.com>
2026-03-03 10:00:55 +0700

    server

--- next.config.ts
+++ next.config.ts
@@ -32,7 +32,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://localhost:8000/api"}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/:path*`,
       },
     ];
   },
